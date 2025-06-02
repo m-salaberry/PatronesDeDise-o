07de97ec1aa0d696cb37d608198e8609484b3bed
Matias Salaberry <matiasfederico.salaberrycapdevila@alumnos.uai.edu.ar>
2025-06-02 19:38:09 -0300

    Singleton

---
+++ PatronesDeDiseño/Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Degregorio";
+
+            Singleton s1 = Singleton.GetIntance(name);
+
+            string name2 = "Martinez";
+
+            Singleton s2 = Singleton.GetIntance(name2);
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works properly");
+            }
+            else
+            {
+                Console.WriteLine("Singleton doesn't work, you must to review your code, dumbass");
+            }
+        }
+    }
+}
