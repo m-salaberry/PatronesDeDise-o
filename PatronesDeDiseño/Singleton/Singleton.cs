475b77bc8a1751560735236a83296fbb5476ccb2
Matias Salaberry <matiasfederico.salaberrycapdevila@alumnos.uai.edu.ar>
2025-06-03 19:30:27 -0300

    Singleton

---
+++ PatronesDeDiseño/Singleton/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+
+        private Singleton() { }
+
+        public string name { get; set; }
+
+        private static Singleton _instance;
+
+        private Singleton(string name)
+        {
+            this.name = name;
+        }
+
+        public static Singleton GetIntance(string name)
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton(name);
+            }
+            return _instance;
+        }
+
+        
+
+    }
+}
