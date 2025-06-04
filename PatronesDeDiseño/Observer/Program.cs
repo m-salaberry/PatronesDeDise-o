2a742a0b8cc40228a620b1e2ca973c7ef719110f
Matias Salaberry <matiasfederico.salaberrycapdevila@alumnos.uai.edu.ar>
2025-06-03 21:40:56 -0300

    Observer

---
+++ PatronesDeDiseño/Observer/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
+
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            subject.SomeBusinessLogic();
+        }
+    }
+}
