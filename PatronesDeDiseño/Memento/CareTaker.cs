0e1a2995a8261f56139ac3a9a833263c3f5b2c7f
Matias Salaberry <matiasfederico.salaberrycapdevila@alumnos.uai.edu.ar>
2025-06-06 14:24:22 -0300

    Memento

---
+++ PatronesDeDiseño/Memento/CareTaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    public class CareTaker
+    {
+
+        private List<Memento> _mementos = new List<Memento>();
+
+        public void Add(Memento m)
+        {
+            _mementos.Add(m);
+        }
+
+        public Memento GetMemento(int index)
+        {
+            return _mementos[index];
+        }
+
+    }
+}
---
+++ PatronesDeDiseño/Memento/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Memento
+{
+    class Program
+    {
+        static CareTaker careTaker = new CareTaker();
+        static void Main(string[] args)
+        {
+            var p = new Persona();
+            p.Nombre = "Pepe";
+
+            careTaker.Add(p.saveToMemento());
+
+            p.Nombre = "Pepe1";
+            careTaker.Add(p.saveToMemento());
+
+            p.Nombre = "Pepe2";
+            careTaker.Add(p.saveToMemento());
+
+            Memento m1 = careTaker.GetMemento(0);
+
+            Console.WriteLine("Viendo memento 1: " + m1.Estado);
+            Console.WriteLine("Viendo memento 2: " + careTaker.GetMemento(1).Estado);
+            Console.WriteLine("Viendo memento 3: " + careTaker.GetMemento(2).Estado);
+
+            Console.WriteLine("\n------");
+            Console.WriteLine(p.Nombre);
+            p.restoreToMemento(m1);
+            Console.WriteLine(p.Nombre);
+
+        }
+    }
+}
