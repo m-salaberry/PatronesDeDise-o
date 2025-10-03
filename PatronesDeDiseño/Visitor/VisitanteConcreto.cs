0ac7aeb42bbe989354d204d3b07e2ba5c6ed9549
Matias Salaberry <matiasfederico.salaberrycapdevila@alumnos.uai.edu.ar>
2025-10-03 18:06:00 -0300

    Visitor pattern v2

--- PatronesDeDiseño/Visitor/VisitanteConcreto.cs
+++ PatronesDeDiseño/Visitor/VisitanteConcreto.cs
@@ -21,5 +21,22 @@ namespace Visitor
             Console.WriteLine($"Visitando Procesador con serial {procesador.Serial}");
         }
     }
-   
+
+    public class VisitanteConcreto2 : IVisitor
+    {
+        public void Visitar(DiscoDuro discoDuro)
+        {
+            Console.WriteLine($"El DiscoDuro tiene el siguiente s/n: {discoDuro.Serial}");
+        }
+
+        public void Visitar(Motherboard motherboard)
+        {
+            Console.WriteLine($"La Motherboard tiene el siguiente s/n: {motherboard.Serial}");
+        }
+
+        public void Visitar(Procesador procesador)
+        {
+            Console.WriteLine($"El Procesador tiene el siguiente s/n: {procesador.Serial}");
+        }
+    }
 }
